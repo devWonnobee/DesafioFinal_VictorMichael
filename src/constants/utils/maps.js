3e30dd964846105006d610d57bb9a6fc93041024
Michael S <39468181+devWonnobee@users.noreply.github.com>
2022-07-17 16:37:33 +0200

    delete APIKEY

--- src/constants/utils/maps.js
+++ src/constants/utils/maps.js
@@ -1,4 +1,4 @@
-const API_KEY_MAPS = 'AIzaSyA7ohwGmPha7d4tF1WUAexraSrFEyN7Q_s';
+const API_KEY_MAPS = '';
 
 export const URL_MAPS = (lat, lng, zoom = 14) => `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=600x300&maptype=roadmap
 &markers=color:blue%7Clabel:S%7C${lat},${lng}&key=${API_KEY_MAPS}`
