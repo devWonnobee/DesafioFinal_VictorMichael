10a683cd39509b1d8f4332ffc26e1358f70eaa7a
Michael S <39468181+devWonnobee@users.noreply.github.com>
2022-07-20 11:31:12 +0200

    delete api
    deleted api key

--- src/constants/database/firebase.js
+++ src/constants/database/firebase.js
@@ -1,5 +1,5 @@
 export const URL_API = "https://restauranteapp-93575-default-rtdb.firebaseio.com/";
-export const API_KEY = "AIzaSyDy0nGEY7pBZ1S1-CQbvyX0v3ggGKi6xAM";
+export const API_KEY = "";
 
 export const URL_AUTH_SIGN_UP = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
 export const URL_AUTH_SIGN_IN = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`;
--- src/constants/utils/maps.js
+++ src/constants/utils/maps.js
@@ -1,4 +1,4 @@
-const API_KEY_MAPS = 'AIzaSyCz3GHjjoC-Urv_QgEiTqa_buaJooMjN54';
+const API_KEY_MAPS = '';
 
 export const URL_MAPS = (lat, lng, zoom = 14) => `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=600x300&maptype=roadmap
 &markers=color:blue%7Clabel:S%7C${lat},${lng}&key=${API_KEY_MAPS}`
